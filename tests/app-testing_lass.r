91704233920819abbf6271aa677a0e9d04fa6b70
Bruno Caneco <bruno@dmpstats.co.uk>
2025-05-16 13:46:41 +0100

    Create app-testing_lass.r

---
+++ tests/app-testing_lass.r
@@ -0,0 +1,58 @@
+# ------------------------- #
+#         Preamble
+# ------------------------- #
+
+library(move2)
+library(httr2)
+library(purrr)
+library(readr)
+library(sf)
+
+# Helpers
+source("tests/app-testing-helpers.r")
+
+
+set_interactive_app_testing()
+
+
+# ---------------------------------------- #
+# ----   Interactive RFunction testing  ----
+# ---------------------------------------- #
+
+test_data <- readRDS(SOME_DATA)
+
+out_dt <- rFunction(data = test_dt)
+out_dt
+
+
+
+
+# ---------------------------------------- #
+# ----            SDK Testing           ----
+# ---------------------------------------- #
+
+run_sdk(data = test_dt, map_output = FALSE)
+read_rds("data/output/output.rds")
+
+
+run_sdk(data = test_dt$nam)
+read_rds("data/output/output.rds")
+
+
+
+# ---------------------------------------- #
+# ----    Automated Unit testing        ----
+# ---------------------------------------- #
+
+# BC: Some test will fail as unit-testing has not been updated to new changes to RFunction()
+
+testthat::test_file("tests/testthat/test_RFunction.R")
+
+
+
+
+
+
+
+
+
