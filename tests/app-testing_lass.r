fcdec04bbbf257c47eaf007be3ef8baeb9a871c7
Lindesay Scott-Hayward <lass@st-and.ac.uk>
2025-05-30 09:57:53 +0100

    Update app-testing_lass.r

--- tests/app-testing_lass.r
+++ tests/app-testing_lass.r
@@ -21,6 +21,10 @@ set_interactive_app_testing()
 
 test_data <- readRDS("C:\\Users\\lass\\University of St Andrews\\MLM_LSHjointwork - Documents\\research\\NCZoo\\Clustering_Callum\\data\\outputdata\\ClusterData_Tanz_POImatch_Jan25_500m_2tablecluster.rds")
 
+test_data <- readRDS("C:\\Users\\lass\\University of St Andrews\\MLM_LSHjointwork - Documents\\research\\NCZoo\\Clustering_Callum\\data\\data\\rawdata\\combine_split_birds_for_kendall_and_wcs__Generate_Avian_Cluster_Metrics__2025-02-17_14-00-42.rds")
+
+test_data <- readRDS("C:\\Users\\lass\\University of St Andrews\\MLM_LSHjointwork - Documents\\research\\NCZoo\\Clustering_Callum\\data\\rawdata\\vulture_study_combine_for_shiny__Generate_Avian_Cluster_Metrics__2025-05-12_13-57-14.rds")
+
 # have to change file path in rFunction for loading data to make this work.  code in rFunction, just need commenting out/in
 out_dt <- rFunction(data = test_data, map_output=FALSE)
 out_dt
